fa3285f1e427d1c2c3b524357933510943241638
nadabishara <nadarawadb@gmail.com>
2025-11-27 13:16:52 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,10 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    
+    final = {}
+    with open(file_path, "r") as y:
+      rows = y.readlines()
+    for r in rows:
+        x = r.strip().split("\t")
+        final[x[0]] = x[2]
 
-
+    return create_codon_dict
